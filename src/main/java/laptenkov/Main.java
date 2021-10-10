1fc9e898e8cdd4e131712867d03c2a7305a5f279
habatoo <habatoo@gmail.com>
2021-10-10 20:09:49 +0700

    MusicBoxJavaAnnotationIml

---
+++ src/main/java/laptenkov/Main.java
@@ -0,0 +1,15 @@
+package laptenkov;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+    MusicBox musicBox = context.getBean("musicBox", MusicBox.class);
+    musicBox.play();
+
+  }
+}
